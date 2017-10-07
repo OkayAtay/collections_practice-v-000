5cd714f44dee09bdc59c2779159e83960dc7b9e8
Araba Taylor <araba.taylor20@gmail.com>
2017-10-07 00:42:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,8 +35,8 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect{|element, index|
     if element[index] != 1
       "#{element}s"
-  end
+  end}
 end
